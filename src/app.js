8e75966054fc1ebcd4712687c193b2c8329173ab
truonganhdung810 <truonganhdung810@gmail.com>
2025-05-16 01:45:51 +0700

    dung bodyParser de URL encode thanh JSON

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import userRoutes from './routes/userRoutes.js'
-import authRoutes from './routes/authRoutes.js'
-import postRoutes from './routes/postRoutes.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/users', userRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/posts', postRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 // Route mẫu
-app.get('/', (req, res) => {
-  res.json({ message: 'API is running' })
-})
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
 
-export default app
+export default app;
