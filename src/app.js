4d99c95e563577401b588761106b52082075053a
truonganhdung810 <truonganhdung810@gmail.com>
2025-05-18 20:52:04 +0700

    token localStorage

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
