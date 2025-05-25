00316dcb33a77854b653f2ab656987240f0d4fc0
truonganhdung810 <truonganhdung810@gmail.com>
2025-05-25 17:33:35 +0700

    update cover done

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,23 +1,21 @@
 // middleware/uploadMiddleware.js
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer')
+const path = require('path')
 
 // Cấu hình nơi lưu trữ file upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log("Cấu hình storage", file);
+    console.log('Cấu hình storage', file)
     // Chỉ định thư mục lưu trữ file
-    cb(null, "./src/uploads/images");
+    cb(null, './src/uploads/images')
   },
   filename: (req, file, cb) => {
-    // Đặt tên file với timestamp + phần mở rộng
-    const ext = path.extname(file.originalname); // Lấy phần mở rộng của file
-    const filename = Date.now() + ext; // Tạo tên file duy nhất
-    cb(null, filename);
+    const filename = Date.now() + file.originalname // Tạo tên file duy nhất
+    cb(null, filename)
   },
-});
+})
 
 // Middleware để upload một file duy nhất
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage })
 
-module.exports = upload;
+module.exports = upload
